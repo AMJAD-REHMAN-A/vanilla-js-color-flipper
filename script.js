3439c1026438d90b6242675a235029f85c2e22b0
Amjad Rehman <mjdrehman@gmail.com>
2023-07-16 15:19:05 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const colors = [
+    "red",
+    "green",
+    "#444444",
+    "rgb(200, 200, 200)"
+]
+
+const btn = document.getElementById("btn-click");
+const color = document.getElementById("color");
+
+btn.addEventListener("click", function() {
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length)
+}
